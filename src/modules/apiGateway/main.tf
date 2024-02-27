1f157d7ef7ac6f2691dbfdf4bbb7418b0751746f
nsosoares <nsosoares@gmail.com>
2024-02-27 15:54:07 -0300

    Remover lambda root

--- src/modules/apiGateway/main.tf
+++ src/modules/apiGateway/main.tf
@@ -5,22 +5,6 @@ resource "aws_api_gateway_rest_api" "tech_lanches_api_gateweay" {
   }
 }
 
-resource "aws_api_gateway_documentation_part" "documentation_part" {
-  location {
-    type = "API"
-  }
-
-  properties  = "{\"description\":\"TechChallenge_APIGatewa\"}"
-  rest_api_id = aws_api_gateway_rest_api.tech_lanches_api_gateweay.id
-}
-
-resource "aws_api_gateway_documentation_version" "documentation" {
-  version = "1.0"
-  rest_api_id = aws_api_gateway_rest_api.tech_lanches_api_gateweay.id
-  description = "TechChallenge - APIGateway"
-  depends_on  = [aws_api_gateway_documentation_part.documentation_part]
-}
-
 resource "aws_api_gateway_resource" "proxy" {
   rest_api_id = "${aws_api_gateway_rest_api.tech_lanches_api_gateweay.id}"
   parent_id   = "${aws_api_gateway_rest_api.tech_lanches_api_gateweay.root_resource_id}"
@@ -43,26 +27,9 @@ resource "aws_api_gateway_integration" "lambda" {
   uri = "${var.arn}"
 }
 
-resource "aws_api_gateway_method" "proxy_root" {
-  rest_api_id = "${aws_api_gateway_rest_api.tech_lanches_api_gateweay.id}"
-  resource_id = "${aws_api_gateway_rest_api.tech_lanches_api_gateweay.root_resource_id}"
-  http_method = "ANY"
-  authorization = "NONE"
-}
-
-resource "aws_api_gateway_integration" "lambda_root" {
-  rest_api_id = "${aws_api_gateway_rest_api.tech_lanches_api_gateweay.id}"
-  resource_id = "${aws_api_gateway_method.proxy_root.resource_id}"
-  http_method = "${aws_api_gateway_method.proxy_root.http_method}"
-  integration_http_method = "POST"
-  type = "AWS_PROXY"
-  uri = "${var.arn}"
-}
-
 resource "aws_api_gateway_deployment" "gateway_deployment" {
   depends_on = [
-    aws_api_gateway_integration.lambda,
-    aws_api_gateway_integration.lambda_root,
+    aws_api_gateway_integration.lambda
   ]
   rest_api_id = "${aws_api_gateway_rest_api.tech_lanches_api_gateweay.id}"
   stage_name = var.environment
